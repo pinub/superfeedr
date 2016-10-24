4f50f68c4d9b704f424008b1c22002e00a5f4c3c
Danilo <d.braband@vaola.de>
2016-10-24 14:44:58 +0200

    New tests with query parameter

--- retrieve_test.go
+++ retrieve_test.go
@@ -3,6 +3,7 @@ package superfeedr
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"reflect"
 	"testing"
 )
@@ -12,11 +13,14 @@ func TestGet(t *testing.T) {
 	defer teardown()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		testQuery(t, r, "hub.mode=retrieve")
+		testQuery(t, r, "hub.topic="+url.QueryEscape("http://www.example.com/test.atom"))
+		testQuery(t, r, "format=json")
 		testMethod(t, r, "GET")
 		fmt.Fprint(w, `{"title":"Example Title","items":[{"id":"tag:1234"},{"id":"tag:5678"}]}`)
 	})
 
-	resp, _, err := client.Retrieve.Get("")
+	resp, _, err := client.Retrieve.Get("http://www.example.com/test.atom")
 	if err != nil {
 		t.Errorf("Retrieve.Get returned error: %v", err)
 	}
